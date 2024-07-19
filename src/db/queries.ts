84ee2f617bffbdea606b57a1ce892fcd435b89a8
Nico Baier <nico.baier@gmail.com>
2024-07-19 17:20:00 -0500

    chore: Refactor upsertLinkMapping function in queries.ts

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,28 +1,28 @@
 import { db } from "./db.ts";
 import {
-  type InsertLink,
-  type SelectLink,
-  linksTable,
-  linkMappingsTable,
-  type InsertLinkMapping,
-  type SelectLinkMapping,
+	type InsertLink,
+	type SelectLink,
+	linksTable,
+	linkMappingsTable,
+	type InsertLinkMapping,
+	type SelectLinkMapping,
 } from "./schema";
-import { type TanaLink } from "../types";
+import type { TanaLink } from "../types";
 import { mergeMappings } from "../utils";
 
 export const getLinks = db.select().from(linksTable);
 
 export const upsertLinkMapping = async (linkMapping: InsertLinkMapping) => {
-  const { mappings: newMappings } = linkMapping;
-  const res = await db.select().from(linkMappingsTable);
-  const existingMappings = res[0]?.mappings;
-  const mergedMappings = mergeMappings(newMappings, existingMappings || []);
-  return db
-    .insert(linkMappingsTable)
-    .values({ id: 1, mappings: mergedMappings })
-    .onConflictDoUpdate({
-      target: linkMappingsTable.id,
-      set: { mappings: mergedMappings },
-    })
-    .returning();
+	const { mappings: newMappings } = linkMapping;
+	const res = await db.select().from(linkMappingsTable);
+	const existingMappings = res[0]?.mappings;
+	const mergedMappings = mergeMappings(newMappings, existingMappings || []);
+	return db
+		.insert(linkMappingsTable)
+		.values({ id: 1, mappings: mergedMappings })
+		.onConflictDoUpdate({
+			target: linkMappingsTable.id,
+			set: { mappings: mergedMappings },
+		})
+		.returning();
 };
