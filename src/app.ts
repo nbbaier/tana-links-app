2faa6e1170e5113e261a478ebe1e4fa43bde1c3f
Nico Baier <nico.baier@gmail.com>
2024-07-22 23:32:02 -0500

    simplify app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,12 @@
-import { db } from "./db/db.ts";
 import { getLinks, upsertLinkMapping } from "./db/queries";
-import { linkMappingsTable } from "./db/schema";
-import type { TanaLink } from "./types";
 import {
 	extractUniqueHostnames,
 	filterMatchingPairs,
 	findWWWDomains,
 } from "./utils";
-import normalizeUrl from "normalize-url";
 
-const results = (await getLinks.execute()) as TanaLink[];
-
-const uniqueHosts = extractUniqueHostnames(results);
+const links = await getLinks();
+const uniqueHosts = extractUniqueHostnames(links);
 const wwwDomains = findWWWDomains(uniqueHosts);
 const matchingPairs = filterMatchingPairs(wwwDomains, uniqueHosts);
 
@@ -22,5 +17,3 @@ try {
 } catch (error) {
 	console.log(error);
 }
-
-console.log((await db.select().from(linkMappingsTable))[0].mappings);
